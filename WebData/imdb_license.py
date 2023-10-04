8bdba840aafc347c8e4195e8cc95bb45632a8765
NormandaleWells <xs9314xk@normandale.edu>
2023-10-04 13:45:33 -0500

    Added instructions to imdb_license.py.

--- WebData/imdb_license.py
+++ WebData/imdb_license.py
@@ -1,6 +1,7 @@
 '''
-Generate your own license key by going to rapidapi.com and signing
-up for an account.
+Generate your own license key by going to rapidapi.com
+and signing up for an account.  Then search for the
+"IMDb" API.  (The capitalization is important.)
 '''
 imdb_key = "<your license key here>"
 imdb_host = "imdb8.p.rapidapi.com"
