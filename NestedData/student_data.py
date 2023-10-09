9a0863a786d6ee57a27fc1341052f77ff6d22da1
NormandaleWells <Richard.Wells@Normandale.edu>
2023-10-09 15:47:44 -0500

    Fleshed out student_data.py.

--- NestedData/student_data.py
+++ NestedData/student_data.py
@@ -6,8 +6,25 @@ def main():
     json_data = f.read()
     f.close()
 
-    student_data = json.loads(json_data)
-    print(student_data)
+    students = json.loads(json_data)
+    #print(json.dumps(students, indent=2))
+    for id,student_data in students.items():
+        assert 'name' in student_data
+        first_name = student_data['name'][0]
+        last_name = student_data['name'][1]
+        assert 'scores' in student_data
+        assignments = student_data['scores']['assignments']
+        exams = student_data['scores']['exams']
+        asgn_avg = sum(assignments) / len(assignments)
+        exams_avg = sum(exams) / len(exams)
+        print(f'{id} {first_name} {last_name} {asgn_avg} {exams_avg}')
+
+    # for id in students:
+    #     print(id)
+    # id = input("Select a student: ")
+    # assert id in students
+    # print(students[id])
+
 
 if __name__ == "__main__":
     main()
