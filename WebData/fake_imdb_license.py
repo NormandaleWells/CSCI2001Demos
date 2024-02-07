3f3b4991ba922138ec2d9a8faa5a1d66a81f6f8d
NormandaleWells <Richard.Wells@Normandale.edu>
2024-02-07 11:35:44 -0600

    Fixed up license file by adding a fake one to show to students.

--- WebData/fake_imdb_license.py
+++ WebData/fake_imdb_license.py
@@ -3,5 +3,5 @@ Generate your own license key by going to rapidapi.com
 and signing up for an account.  Then search for the
 "IMDb" API.  (The capitalization is important.)
 '''
-imdb_key = "d4951dd982msh7326f617610bcb8p1bbb56jsndd0d70a05021"
+imdb_key = "??????"
 imdb_host = "imdb8.p.rapidapi.com"
